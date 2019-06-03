08751ce646226b74242d93940b09d9629d55d72b
Ott Kuulme <ott.kuulme@tptlive.ee>
2019-06-03 14:09:37 +0300

    Canvasi kasutamine

--- snakeXD/snakeXD/MainWindow.xaml.cs
+++ snakeXD/snakeXD/MainWindow.xaml.cs
@@ -24,5 +24,46 @@ namespace snakeXD
         {
             InitializeComponent();
         }
+
+        private void Canvas_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Right)
+            {
+                double currentRight = Canvas.GetRight(Jens);
+                double newRight = currentRight + 20;
+                Canvas.SetRight(Jens, newRight);
+            }
+
+            if (e.Key == Key.Left)
+            {
+                double currentLeft = Canvas.GetLeft(Jens);
+                double newLeft = currentLeft + 20;
+                Canvas.SetLeft(Jens, newLeft);
+            }
+
+            if (e.Key == Key.Up)
+            {
+                double currentUp = Canvas.GetTop(Jens);
+                double newUp = currentUp + 20;
+                Canvas.SetTop(Jens, newUp);
+            }
+
+            if(e.Key == Key.Down)
+            {
+                double currentDown = Canvas.GetBottom(Jens);
+                double newBottom = currentDown + 20;
+                Canvas.SetBottom(Jens, newBottom);
+            }
+        }
     }
 }
